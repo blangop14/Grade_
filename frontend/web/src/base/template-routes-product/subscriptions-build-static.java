d2bdc0f2f07e53d312e3c3e02abeeef953fd18ce
blangop14 <blangop14@users.noreply.github.com>
2025-11-08 17:00:50 +0000

    Add message.toml, order-environment.env, subscriptions-build-static.java

---
+++ frontend/web/src/base/template-routes-product/subscriptions-build-static.java
@@ -0,0 +1,4 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/grade_/frontend/web/src/base/template-routes-product/subscriptions-build-static.java
+// Generated at: 2025-11-27T15:10:54.544Z
+
+
